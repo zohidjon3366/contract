2a4034e94fc52fab24f2383e153a32fda66986e9
zohidjon3366 <zohidjon3366@gmail.com>
2026-07-07 15:59:14 +0500

    Add files via upload

---
+++ FINAL_V10_5_LIGHT_THEME.md
@@ -0,0 +1,11 @@
+# ALL FINANCE CONTRACT FINAL v10.5
+
+## Dizayn yangilanishlari
+
+- Foydalanuvchi yuborgan All Finance logotipi qo‘llandi.
+- Login oynasida to‘liq logo, yon panelda ixcham ikon variant ishlatiladi.
+- Platforma ranglari ochroq, zamonaviy va yengil ko‘rinishga o‘tkazildi.
+- Yon panel to‘q fon o‘rniga yengil brand uslubdagi oq-ko‘kish gradientga o‘tdi.
+- Shriftlar `Manrope` va `Inter` zanjiriga o‘tkazildi.
+- Tugmalar, kartalar, jadvallar va badge elementlari ochroq dizaynda qayta uslublandi.
+- Asosiy All Finance binafsha-yashil rang yo‘nalishi saqlab qolindi.
--- server.js
+++ server.js
@@ -83,7 +83,7 @@ const server = http.createServer((req, res) => {
   const pathname = requestUrl.pathname;
 
   if (pathname === '/health') {
-    sendJson(res, 200, { ok: true, app: 'ALL FINANCE CONTRACT FINAL v10.4' });
+    sendJson(res, 200, { ok: true, app: 'ALL FINANCE CONTRACT FINAL v10.5' });
     return;
   }
 
@@ -112,5 +112,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, HOST, () => {
-  console.log(`ALL FINANCE CONTRACT FINAL v10.4 running on http://${HOST}:${PORT}`);
+  console.log(`ALL FINANCE CONTRACT FINAL v10.5 running on http://${HOST}:${PORT}`);
 });
